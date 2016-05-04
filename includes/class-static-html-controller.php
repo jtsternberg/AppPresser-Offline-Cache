a44194b8792cc49800c4df2be378a7963686bea4
Justin Sternberg <justin@dsgnwrks.pro>
2016-05-04 12:08:15 -0400

    Add filters for the get_static_html call

--- includes/class-static-html-controller.php
+++ includes/class-static-html-controller.php
@@ -248,9 +248,11 @@ class APOC_Static_HTML_Controller extends WP_REST_Posts_Controller {
 	 * @return string       HTML output of URL.
 	 */
 	public function get_static_html( $url ) {
-		$html = wp_remote_retrieve_body( wp_remote_get( $url ) );
+		$args = apply_filters( 'apppresser_offline_cache_static_html_request_args', array() );
+		$url  = apply_filters( 'apppresser_offline_cache_static_html_request_url', $url );
+		$html = wp_remote_retrieve_body( wp_remote_get( $url, $args ) );
 
-		return $html;
+		return apply_filters( 'apppresser_offline_cache_static_html_request_html', $html );
 	}
 
 	/**
